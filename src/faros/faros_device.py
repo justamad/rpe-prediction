796075f60aaa23557f19a03b7d0959fc5e65bdec
Justin Albert <justin.albert@hpi.de>
2021-03-05 14:48:17 +0100

    Added biosppy heart rate calculation to faros device class.

--- src/faros/faros_device.py
+++ src/faros/faros_device.py
@@ -1,7 +1,6 @@
-from typing import List, Tuple
-
-from src.processing import apply_butterworth_filter, normalize_signal, find_peaks, utils
+from src.processing import apply_butterworth_filter, normalize_signal, find_peaks
 from pyedflib import highlevel
+from biosppy.signals import ecg
 
 import pandas as pd
 import numpy as np
@@ -15,19 +14,20 @@ class Faros(object):
             raise Exception(f"Folder {folder} does not exist.")
 
         file_name = os.path.join(folder, "record.edf")
-        self.acc_data = self.read_acceleration_data_from_file(file_name).iloc[start:end]
         self.file_name = file_name
-        self._sampling_frequency = self.read_acceleration_freq_from_file(file_name)
+        self.acc_data = self.read_acceleration_data_from_file().iloc[start:end]
+        self.ecg_data = self.read_ecg_signal()
+        self._sampling_frequency = self.read_acceleration_freq_from_file()
+        self._sampling_frequency_ecg = self.read_ecg_freq_from_file()
+
+        # Peak finding parameters
         self.height = 1.2
         self.prominence = 1.0
         self.distance = None
 
-    def get_acceleration_data(self, add_magnitude=False) -> List[Tuple[np.ndarray, str]]:
+    def get_acceleration_data(self):
         data_np = self.acc_data.to_numpy()
-        if add_magnitude:
-            return [(utils.add_magnitude(data_np), "Faros")]
-        else:
-            return [(data_np, "Faros")]
+        return data_np
 
     def get_synchronization_signal(self) -> np.ndarray:
         data = self.acc_data['Accelerometer_X'].to_numpy()
@@ -44,17 +44,26 @@ class Faros(object):
         peaks = find_peaks(-processed_signal, height=self.height, prominence=self.prominence)
         return clock, raw_signal, processed_signal, peaks
 
-    @staticmethod
-    def read_acceleration_freq_from_file(file_name):
-        _, signal_headers, _ = highlevel.read_edf(file_name)
+    def get_heart_rate_signal(self):
+        heart_rate = ecg.ecg(self.ecg_data, sampling_rate=self._sampling_frequency_ecg, show=False)
+        return heart_rate['heart_rate_ts'], heart_rate['heart_rate']
+
+    def read_acceleration_freq_from_file(self):
+        _, signal_headers, _ = highlevel.read_edf(self.file_name)
         sample_rates = list(map(lambda h: h["sample_rate"], filter(lambda h: "Acc" in h["label"], signal_headers)))
         if any([sr != sample_rates[0] for sr in sample_rates]):
             raise UserWarning(f"Not all Faros accelerometer sampling rates are the same: {sample_rates}")
+
         return sample_rates[0]
 
-    @staticmethod
-    def read_acceleration_data_from_file(file_name):
-        signals, signal_headers, header = highlevel.read_edf(file_name)
+    def read_ecg_freq_from_file(self):
+        _, signal_header, _ = highlevel.read_edf(self.file_name)
+        sampling_freq = [signal['sample_rate'] for signal in signal_header if signal['label'] == 'ECG']
+        assert len(sampling_freq) == 1, f"No or too many ECG signals: {sampling_freq}"
+        return sampling_freq[0]
+
+    def read_acceleration_data_from_file(self):
+        signals, signal_headers, header = highlevel.read_edf(self.file_name)
 
         headers = ['Accelerometer_X', 'Accelerometer_Y', 'Accelerometer_Z']
         data = []
@@ -65,9 +74,13 @@ class Faros(object):
         data_body = np.stack(data, axis=1)
         return pd.DataFrame(data_body, columns=headers)
 
-    @staticmethod
-    def get_signal_from_datasource(file_name, label):
-        signals, signal_headers, header = highlevel.read_edf(file_name)
+    def read_ecg_signal(self):
+        signals, signal_headers, _ = highlevel.read_edf(self.file_name)
+        ecg_signal = [signal['label'] for signal in signal_headers].index("ECG")
+        return signals[ecg_signal]
+
+    def get_signal_from_datasource(self, label):
+        signals, signal_headers, header = highlevel.read_edf(self.file_name)
         idx = [header['label'] for header in signal_headers].index(label)
         return signals[idx]
 
