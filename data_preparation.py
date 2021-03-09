1bd16567e349369837ccabc64a07508d451e4650
Justin Albert <justin.albert@hpi.de>
2021-03-09 17:28:46 +0100

    Added data from Bjarne Trial to data directory. Possible bug in synchronization.

--- data_preparation.py
+++ data_preparation.py
@@ -15,17 +15,21 @@ parser.add_argument('--src_path', type=str, dest='src_path', default="data/bjarn
 parser.add_argument('--report_path', type=str, dest='report_path', default="reports")
 args = parser.parse_args()
 
-if os.path.exists(args.report_path):
-    shutil.rmtree(args.report_path)
-os.makedirs(args.report_path)
+
+def clean_up_dirs(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.makedirs(directory)
+
 
 # Load data
 config = ConfigReader(join(args.src_path, "config.json"))
-faros = Faros(join(args.src_path, "faros"))
+# faros = Faros(join(args.src_path, "faros"))
 gaitup = GaitUp(join(args.src_path, "gaitup"))
-exit(-1)
+
 # Process individual sets
 for counter, sensor_trial in enumerate(config.iterate_over_trials()):
+    print(f"Convert trial {counter}...")
     # faros_set = faros.cut_trial(*sensor_trial['faros'])
 
     # Map indices to according sampling frequency
@@ -33,9 +37,10 @@ for counter, sensor_trial in enumerate(config.iterate_over_trials()):
     start_idx = int(start_idx * (100 / 128))
     end_idx = int(end_idx * (100 / 128))
     gaitup_trial = gaitup.cut_data(start_idx, end_idx)
-    azure = AzureKinect(join(args.src_path, f"azure/0{counter + 1}_sub/positions_3d.csv"))
+    azure = AzureKinect(join(args.src_path, "azure",  f"0{counter + 1}_sub", "positions_3d.csv"))
 
     # Synchronize signals
-    azure, gaitup = synchronize_signals(azure, gaitup_trial, method="correlation", show=True, path=args.report_path)
-    print(len(azure.data), len(gaitup.data))
-    break
+    report_path = join(args.report_path, f"{counter}_azure")
+    clean_up_dirs(report_path)
+    azure, gaitup_trial = synchronize_signals(azure, gaitup_trial, method="correlation", show=True, path=report_path)
+    print(len(azure.data), len(gaitup_trial.data))
