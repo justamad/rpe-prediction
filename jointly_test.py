f5651558e1df0480075016825c79fecf6999f1b9
JustinAlbert <justin_albert@gmx.de>
2021-04-19 22:41:02 +0200

    Added entire jointly package into own code.

--- jointly_test.py
+++ jointly_test.py
@@ -1,31 +1,27 @@
 from src.processing import calculate_magnitude
+from src.jointly import ShakeExtractor, Synchronizer
 
 import matplotlib
-matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-
-import jointly
 import pandas as pd
-import datetime
+
+matplotlib.use("TkAgg")
 
 
 def read_csv_and_convert_timestamp(file_name):
     df = pd.read_csv(file_name, sep=';', index_col=False)
-    index = pd.DatetimeIndex([datetime.datetime.fromtimestamp(t) for t in df['sensorTimestamp']])
-    df = df.drop(['sensorTimestamp'], axis=1).set_index(index)
-
+    df["sensorTimestamp"] = pd.to_datetime(df["sensorTimestamp"], unit="s")
+    df = df.set_index("sensorTimestamp", drop=True)
+    index = df.index
     # Calculate magnitude
-    m = calculate_magnitude(df, "_X")
-    m = m.set_index(index)
-    return m
+    df = calculate_magnitude(df, "_X")
+    df = df.set_index(index)
+    return df
 
 
 p_1 = read_csv_and_convert_timestamp("625.csv")
 print(p_1)
-# plt.plot(p_1['ACCELERATION'])
-# plt.show()
 
-# exit(-1)
 p_2 = read_csv_and_convert_timestamp("629.csv")
 print(p_1)
 
@@ -41,6 +37,6 @@ sources = {
 
 ref_source_name = 'IMU_1'
 
-extractor = jointly.ShakeExtractor()
-synchronizer = jointly.Synchronizer(sources, ref_source_name, extractor)
+extractor = ShakeExtractor()
+synchronizer = Synchronizer(sources, ref_source_name, extractor)
 synced_data = synchronizer.get_synced_data()
