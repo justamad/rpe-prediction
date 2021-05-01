8250150519f266368b76682e848b3616096699fb
Justin <mail@justin-albert.com>
2021-05-01 13:12:07 +0200

    Optimized ts feature calculation. Implemented stride in sliding window method.

--- src/features/sliding_window.py
+++ src/features/sliding_window.py
@@ -1,8 +1,9 @@
 from tsfresh.feature_extraction import MinimalFCParameters
-from src.processing import get_joints_as_list, reshape_data_for_ts, reshape_data_from_ts
+from src.processing import get_joints_as_list
 
 import tsfresh
 import pandas as pd
+import math
 
 settings = MinimalFCParameters()
 
@@ -18,16 +19,24 @@ def calculate_features_sliding_window(df: pd.DataFrame, window_size: int, step_s
     joints = get_joints_as_list(df, " (x) pos")
     joints.remove('t')  # TODO: Change this
 
-    window_features = []
+    windows = []
 
-    length = len(df) - window_size + 1
-    print(f"Calculated Windows: {length}")
-    for window in range(length):
-        data = df[window:window + window_size - 1]
-        data = reshape_data_for_ts(data, joints)
-        feat = tsfresh.extract_features(data, column_id='id', column_sort='timestamp', default_fc_parameters=settings)
-        result = reshape_data_from_ts(feat)
-        window_features.append(result)
+    n_windows = math.floor((len(df) - window_size) / step_size) + 1
+    print(f"Calculated Windows: {n_windows}")
+    for window_id, window_index in enumerate(range(0, n_windows, step_size)):
+        window = df[window_index:window_index + window_size - 1].copy()
+        window['id'] = window_id
+        windows.append(window)
 
-    df = pd.concat(window_features, ignore_index=True)
-    return df
+    df = pd.concat(windows, ignore_index=True)
+    return calculate_ts_features(df)
+
+
+def calculate_ts_features(df):
+    """
+    Calculate features for entire dataframe that contains window index with column 'id'
+    @param df: dataframe for all possible windows
+    @return: calculated features
+    """
+    feat = tsfresh.extract_features(df, column_id='id', column_sort='timestamp', default_fc_parameters=settings)
+    return feat
--- train_svr.py
+++ train_svr.py
@@ -1,6 +1,7 @@
 from sklearn.svm import SVR
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from src.processing import normalize_into_interval
 
 import pandas as pd
 import matplotlib
@@ -9,23 +10,32 @@ import numpy as np
 
 matplotlib.use("TKAgg")
 
+sc_x = StandardScaler()
+sc_y = StandardScaler()
 
 X_train = pd.read_csv("X_train.csv", sep=";").to_numpy()
-y_train = pd.read_csv("y_train.csv", sep=";").to_numpy().reshape(-1)
+y_train = pd.read_csv("y_train.csv", sep=";").to_numpy().reshape(-1, 1)
 
 X_test = pd.read_csv("X_test.csv", sep=";").to_numpy()
-y_test = pd.read_csv("y_test.csv", sep=";").to_numpy().reshape(-1)
+y_test = pd.read_csv("y_test.csv", sep=";").to_numpy().reshape(-1, 1)
+
+X_train = sc_x.fit_transform(X_train)
+y_train = normalize_into_interval(y_train, -1, 1)
+
+X_test = sc_x.fit_transform(X_test)
+y_test = normalize_into_interval(y_test, -1, 1)
 
 print(X_train.shape, y_train.shape)
 print(X_test.shape, y_test.shape)
-svr = make_pipeline(StandardScaler(), SVR(kernel='rbf', C=2.0, epsilon=0.3))
-svr.fit(X_train, y_train)
-print(svr.score(X_train, y_train))
+# svr = make_pipeline(StandardScaler(), SVR(kernel='rbf', C=2.0, epsilon=0.3))
+svr = SVR(kernel='rbf', C=3.0, epsilon=0.01)
+svr.fit(X_test, y_test)
+print(svr.score(X_test, y_test))
 
-pred_y = svr.predict(X_test)
-plt.plot(y_test, label="Ground Truth")
+pred_y = svr.predict(X_train)
+plt.plot(y_train, label="Ground Truth")
 plt.plot(pred_y, label="Predictions")
 plt.legend()
 plt.tight_layout()
 # plt.show()
-plt.savefig("output_inv.png")
+plt.savefig("output.png")
