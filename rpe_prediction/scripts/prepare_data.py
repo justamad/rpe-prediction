Continued Stereo Azure Kinect. Fixed some bugs and implemented average fusion.

rpe_prediction/scripts/prepare_data.py
@@ -2,22 +2,20 @@ from rpe_prediction.devices import AzureKinect
 from rpe_prediction.features import calculate_features_sliding_window
 from rpe_prediction.plot import plot_sensor_data_for_axes
 from rpe_prediction.config import RawDataIterator
-from rpe_prediction.processing import filter_dataframe, normalize_mean, segment_exercises_based_on_joint
+from rpe_prediction.processing import filter_dataframe, segment_exercises_based_on_joint
 
 import numpy as np
 import pandas as pd
-import matplotlib
 
-# matplotlib.use("TkAgg")
 excluded_joints = ["eye", "ear", "nose", "wrist", "hand", "thumb"]
-file_iterator = RawDataIterator("data/raw")
+file_iterator = RawDataIterator("../../data/raw")
 
 
 def prepare_skeleton_data(iterator):
     """
     Prepare Kinect skeleton data using the RawFileIterator
     @param iterator: RawFileIterate that delivers all sets over all subjects
-    @return:
+    @return: A tuple of pandas data frames: (input, labels)
     """
     x_data, y_labels = [], []
     groups = []
@@ -30,22 +28,22 @@ def prepare_skeleton_data(iterator):
         azure.filter_data(order=4)
         data = filter_dataframe(azure._data, excluded_joints)
         data = filter_dataframe(data, ["ori"])  # TODO: Fix, add timestamps to position_data?
-        # data = normalize_mean(data, std_dev_factor=1.0)
-        # plot_sensor_data_for_axes(data, "Position_data", azure.get_skeleton_joints(), f"{set_data['azure']}_pos.png")
+        plot_sensor_data_for_axes(data, "Position_data", azure.get_skeleton_joints(), f"{set_data['azure']}_pos.png")
         # segment_exercises_based_on_joint(positions[], )
+
         # features = calculate_features_sliding_window(data, window_size=30, step_size=2)
         # x_data.append(features)
         # y_labels.extend([set_data['rpe'] for _ in range(len(features))])
         # groups.extend([set_data['group'] for _ in range(len(features))])
 
     X = pd.concat(x_data, ignore_index=True)
-    y = pd.DataFrame(np.stack([y_labels, groups], axis=1), columns=["rpe", "groups"])
-    X.to_csv("x.csv", index=False, sep=';')
-    y.to_csv("y.csv", index=False, sep=';')
-
-    print(X.shape)
-    print(y.shape)
+    y = pd.DataFrame(np.stack([y_labels, groups], axis=1), columns=["rpe", "group"])
+    return X, y
 
 
 if __name__ == '__main__':
-    prepare_skeleton_data(file_iterator.iterate_over_all_subjects())
+    X, y = prepare_skeleton_data(file_iterator.iterate_over_all_subjects())
+    print(X.shape)
+    print(y.shape)
+    X.to_csv("x.csv", index=False, sep=';')
+    y.to_csv("y.csv", index=False, sep=';')

rpe_prediction/stereo_cam/stereo_azure.py
@@ -1,24 +1,33 @@
 from rpe_prediction.devices import AzureKinect
-from rpe_prediction.rendering import SkeletonViewer
+from rpe_prediction.processing import apply_butterworth_filter_dataframe
 from .icp import find_rigid_transformation_svd
 
 import numpy as np
+import pandas as pd
 import open3d as o3d
 import copy
+import matplotlib.pyplot as plt
 
 
 class StereoAzure(object):
 
     def __init__(self, master_path, sub_path, delay=0.001):
+        # Read in master device
         self.master = AzureKinect(master_path)
-        self.sub = AzureKinect(sub_path)
         self.master.process_raw_data()
-        self.sub.process_raw_data()
-        self.delay = delay
 
+        # Read in sub device
+        self.sub = AzureKinect(sub_path)
+        self.sub.process_raw_data()
+
+        self.delay = delay
         self.time_synchronization()
 
-        # self.spatial_alignment()
+        self.sub_pos = self.sub.position_data
+        self.mas_pos = self.master.position_data
+
+        self.avg_df = (self.sub_pos + self.mas_pos) / 2
+        # self.avg_df = apply_butterworth_filter_dataframe(self.avg_df, sampling_frequency=30)
 
     def time_synchronization(self):
         # Synchronize master and sub devices
@@ -35,6 +44,26 @@ class StereoAzure(object):
 
     def apply_external_rotation(self, rotation, translation):
         self.sub.multiply_matrix(rotation, translation)
+
+    def plot_axis(self):
+        row, cols = self.sub_pos.shape
+
+        for i in range(cols):
+            column_name = self.sub_pos.columns[i]
+            sub_data = self.sub_pos[column_name]
+            mas_data = self.mas_pos[column_name]
+            diff = np.abs(sub_data - mas_data)
+            avg = self.avg_df[column_name]
+
+            plt.plot(sub_data, label="sub")
+            plt.plot(mas_data, label="master")
+            plt.plot(diff, label="diff")
+            plt.plot(avg, label="Average")
+            plt.ylabel("MM")
+            plt.xlabel("Frames")
+            plt.title(f"{column_name} - Diff: {np.mean(diff)}, {np.std(diff)}")
+            plt.legend()
+            plt.show()
 
     def spatial_alignment(self):
         master_position = self.master.position_data.to_numpy()[1500:1600, :]