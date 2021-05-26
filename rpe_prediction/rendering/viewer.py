Implemented scaling in skeleton viewer. Possible bug in skeleton fusion.

rpe_prediction/rendering/viewer.py
@@ -1,4 +1,6 @@
 import vtk
+import pandas as pd
+import numpy as np
 
 COLORS = ["red", "green", "blue"]
 
@@ -6,6 +8,9 @@ COLORS = ["red", "green", "blue"]
 class SkeletonViewer(object):
 
     def __init__(self):
+        """
+        Skeleton Viewer that uses the VTK graphic framework
+        """
         self.colors = vtk.vtkNamedColors()
         self.renderer = vtk.vtkRenderer()
         self.renderer.SetBackground(0, 0, 0)
@@ -21,12 +26,14 @@ class SkeletonViewer(object):
         self.render_window_interactor.AddObserver('KeyPressEvent', self.keypress_callback, 1.0)
 
         # Data objects
-        self._skeleton_objects = []
-        self._max_frames = 1e10
+        self.__skeleton_objects = []
+        self.__max_frames = float('inf')
         self.__cur_frame = 0
         self.__break = False
+        self.__trans_vector = np.array([0, 0, 0])
+        self.__scale_factor = 1.0
 
-    def add_skeleton(self, data, connections=None, radius: float = 0.02):
+    def add_skeleton(self, data: pd.DataFrame, connections=None, radius: float = 0.02):
         """
         Add a new skeleton to the renderer
         @param data: new skeleton data in a numpy array
@@ -37,9 +44,7 @@ class SkeletonViewer(object):
         actors_markers = []  # each marker has an own actor
         actors_bones = []  # actors for each line segment between two markers
         lines = []
-        rows, cols = data.shape
-        if rows < self._max_frames:
-            self._max_frames = rows  # set max size to smallest video length
+        _, cols = data.shape
 
         # Create all instances for all markers
         for marker in range(cols // 3):
@@ -52,7 +57,7 @@ class SkeletonViewer(object):
             mapper.AddInputConnection(sphere.GetOutputPort())
             actor = vtk.vtkActor()
             actor.SetMapper(mapper)
-            actor.GetProperty().SetColor(self.colors.GetColor3d(COLORS[len(self._skeleton_objects)]))
+            actor.GetProperty().SetColor(self.colors.GetColor3d(COLORS[len(self.__skeleton_objects)]))
             self.renderer.AddActor(actor)
             actors_markers.append(actor)
 
@@ -67,29 +72,40 @@ class SkeletonViewer(object):
                 mapper.AddInputConnection(line.GetOutputPort())
                 actor = vtk.vtkActor()
                 actor.SetMapper(mapper)
-                actor.GetProperty().SetColor(self.colors.GetColor3d(COLORS[len(self._skeleton_objects)]))
+                actor.GetProperty().SetColor(self.colors.GetColor3d(COLORS[len(self.__skeleton_objects)]))
                 self.renderer.AddActor(actor)
                 actors_bones.append(actor)
 
-        self._skeleton_objects.append({
-            'data': data,
+        # Invert y-coordinate axis for Azure Kinect data
+        data[data.filter(like='(y)').columns] *= -1
+
+        self.__skeleton_objects.append({
+            'data': data.to_numpy(),
             'connections': connections,
             'lines': lines,
             'actors_markers': actors_markers,
         })
 
-    def show_window(self, scale: float = 0.5):
+    def show_window(self, axis_scale: float = 0.3):
         """
         Start the rendering sequence
-        @param scale: scaling factor
+        @param axis_scale: scaling factor for coordinate system axes
         @return: None
         """
         # Initialize a timer for the animation
         self.render_window_interactor.AddObserver('TimerEvent', self.update)
+        self.__max_frames = min(map(lambda x: len(x['data']), self.__skeleton_objects))
+
+        # Calculate bounding box and scale to center
+        data = np.concatenate(list(map(lambda x: x['data'].reshape(-1, 3), self.__skeleton_objects)))
+        min_vals = np.min(data, axis=0)
+        max_vals = np.max(data, axis=0)
+        self.__trans_vector = np.array([(min_vals[0] + max_vals[0]) / 2, min_vals[1], (min_vals[2] + max_vals[2]) / 2])
+        self.__scale_factor = np.max(data - self.__trans_vector)
 
         # create coordinate actor
         axes = vtk.vtkAxesActor()
-        axes.SetTotalLength(scale, scale, scale)
+        axes.SetTotalLength(axis_scale, axis_scale, axis_scale)
         axes.GetXAxisCaptionActor2D().GetTextActor().SetTextScaleModeToNone()
         axes.GetYAxisCaptionActor2D().GetTextActor().SetTextScaleModeToNone()
         axes.GetZAxisCaptionActor2D().GetTextActor().SetTextScaleModeToNone()
@@ -101,15 +117,15 @@ class SkeletonViewer(object):
         self.render_window_interactor.Start()
 
     def update(self, obj, event):
-        if self.__cur_frame >= self._max_frames:
+        if self.__cur_frame >= self.__max_frames:
             self.__cur_frame = 0
 
         # Draw individual skeleton
-        for skeleton_data in self._skeleton_objects:
+        for skeleton_data in self.__skeleton_objects:
             # Update marker position for current frame
             data = skeleton_data['data']
             actors_markers = skeleton_data['actors_markers']
-            points = data[self.__cur_frame].reshape(-1, 3)
+            points = (data[self.__cur_frame].reshape(-1, 3) - self.__trans_vector) / self.__scale_factor
 
             for c_points, actor in enumerate(actors_markers):
                 x, y, z = points[c_points]
@@ -139,5 +155,5 @@ class SkeletonViewer(object):
             print(f"Current Frame: {self.__cur_frame}")
         elif key == 'Right':
             new_frame = self.__cur_frame + 1
-            self.__cur_frame = new_frame if new_frame < self._max_frames else self.__cur_frame
+            self.__cur_frame = new_frame if new_frame < self.__max_frames else self.__cur_frame
             print(f"Current Frame: {self.__cur_frame}")

rpe_prediction/stereo_cam/stereo_azure.py
@@ -40,13 +40,14 @@ class StereoAzure(object):
         """
         self.sub.multiply_matrix(rotation, translation)
 
-    def calculate_spatial_on_data(self):
+    def calculate_spatial_on_data(self, show=False):
         master_position = self.master.position_data.to_numpy()  # [400:600, :]
         sub_position = self.sub.position_data.to_numpy()  # [400:600, :]
 
-        # Spatial alignment
+        # Find the best affine transformation
         rotation, translation = find_rigid_transformation_svd(master_position.reshape(-1, 3),
-                                                              sub_position.reshape(-1, 3), True)
+                                                              sub_position.reshape(-1, 3),
+                                                              show=show)
 
         trans_init = np.eye(4)
         trans_init[0:3, 0:3] = rotation
@@ -54,18 +55,16 @@ class StereoAzure(object):
         self.master.multiply_matrix(rotation, translation)
 
     @staticmethod
-    def calculate_percentage_df(grad_a, grad_b):
-        shape = grad_a.shape
-        mat = np.stack([grad_a, grad_b], axis=2)
-        sums = np.sum(mat, axis=2).reshape((shape[0], shape[1], 1))
-        mat = mat / sums
-        return 1 - mat
+    def calculate_percentage(grad_a, grad_b):
+        rows, cols = grad_a.shape
+        gradient_stack = np.stack([grad_a, grad_b], axis=2)
+        sums = np.sum(gradient_stack, axis=2).reshape((rows, cols, 1))
+        return gradient_stack / sums
 
-    def calculate_fusion(self, alpha, beta, window_size=5):
+    def calculate_fusion(self, alpha, window_size=5):
         """
         Calculate the fusion of sub and master cameras. Data should be calibrated as good as possible
         @param alpha: coefficient for dominant skeleton side
-        @param beta: coefficient for weaker skeleton side
         @param window_size: a window size of gradient averages
         @return: Fused skeleton data in a pandas array
         """
@@ -76,8 +75,9 @@ class StereoAzure(object):
         grad_b = np.square(np.gradient(self.mas_position.to_numpy(), axis=0))
         grad_a = pd.DataFrame(grad_a).rolling(window=window_size, min_periods=1, center=True).mean()
         grad_b = pd.DataFrame(grad_b).rolling(window=window_size, min_periods=1, center=True).mean()
-        gradient_weights = self.calculate_percentage_df(grad_a, grad_b)
-        fused_skeleton = alpha * gradient_weights[:, :, 0] * df_sub + beta * gradient_weights[:, :, 1] * df_master
+        weights = self.calculate_percentage(grad_a, grad_b)
+        fused_skeleton = (1 - weights[:, :, 0] + alpha) * df_sub + (1 - weights[:, :, 1] - alpha) * df_master
+        # fused_skeleton = (0.5 + alpha) * df_sub + (0.5 - alpha) * df_master
         return fused_skeleton
 
     @property