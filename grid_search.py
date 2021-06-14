ad10f1e222213f80f36aceeaf4fdd3aba0c880e6
Justin Albert <justin.albert@hpi.de>
2021-06-14 12:37:58 +0200

    Implemented RFECV into gridsearch module.

--- grid_search.py
+++ grid_search.py
@@ -1,9 +1,15 @@
 from rpe_prediction.config import SubjectDataIterator, FusedAzureLoader, RPELoader
 from rpe_prediction.models import GridSearching, SVRModelConfig, split_data_to_pseudonyms
+from sklearn.feature_selection import RFECV
+from sklearn.svm import SVR
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.preprocessing import StandardScaler
 from datetime import datetime
 from os.path import join
 
 import numpy as np
+import pandas as pd
 import prepare_data
 import argparse
 import logging
@@ -20,18 +26,19 @@ logging.getLogger('my_logger').addHandler(console)
 parser = argparse.ArgumentParser()
 parser.add_argument('--src_path', type=str, dest='src_path', default="data/processed")
 parser.add_argument('--out_path', type=str, dest='out_path', default="results")
+parser.add_argument('--nr_features', type=int, dest='nr_features', default=40)
 args = parser.parse_args()
 
 out_path = join(args.out_path, datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
-
 if not os.path.exists(out_path):
     os.makedirs(out_path)
 
 window_sizes = [30, 60, 90]
-step_sizes = [5, 10]
+step_sizes = [10, 15, 20]
 file_iterator = SubjectDataIterator(args.src_path).add_loader(RPELoader).add_loader(FusedAzureLoader)
 
 models = [SVRModelConfig()]
+logo = LeaveOneGroupOut()
 
 # Iterate over non-sklearn hyperparameters
 for window_size in window_sizes:
@@ -48,6 +55,33 @@ for window_size in window_sizes:
         # Save train and test subjects to file
         np.savetxt(join(out_path, f"train_win_{window_size}_step_{step_size}.txt"), y_train['name'].unique(), fmt='%s')
         np.savetxt(join(out_path, f"test_win_{window_size}_step_{step_size}.txt"), y_test['name'].unique(), fmt='%s')
+
+        # Perform an initial recursive feature elimination
+        rfecv = RFECV(SVR(kernel='linear'),
+                      min_features_to_select=args.nr_features,
+                      step=0.1,
+                      n_jobs=-1,
+                      verbose=10,
+                      cv=logo.get_n_splits(groups=y_train_group))
+
+        # selector = RFE(estimator, n_features_to_select=30, step=20, verbose=10)
+        pipe = Pipeline([
+            ('scaler', StandardScaler()),
+            ('rfecv', rfecv)
+        ])
+
+        pipe.fit(X_train, y_train_rpe)
+        logging.info(pipe)
+        logging.info(pipe.score(X_test, y_test_rpe))
+
+        # Save RFECV results for later
+        rfecv_df = pd.DataFrame(rfecv.ranking_, index=X.columns, columns=['Rank']).sort_values(by='Rank', ascending=True)
+        rfecv_df.head()
+        rfecv_df.to_csv(join(out_path, f"win_{window_size}_step_{step_size}.csv"))
+
+        # Only use the n most significant features
+        X_train = X_train[rfecv.support_]
+        X_test = X_test[rfecv.support_]
 
         # Iterate over models and perform Grid Search
         for model_config in models:
