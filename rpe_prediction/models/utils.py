bb5be8cd14e6e3d7c769666354c4006b4cf05daa
Justin Albert <justin.albert@hpi.de>
2021-06-17 13:41:34 +0200

    Changed random_seed from bool to number. Implemented Nan and Inf check.

--- rpe_prediction/models/utils.py
+++ rpe_prediction/models/utils.py
@@ -3,20 +3,20 @@ import pandas as pd
 
 
 def split_data_to_pseudonyms(x: pd.DataFrame, y: pd.DataFrame, train_percentage: float = 0.8,
-                             random_seed: bool = False):
+                             random_seed: int = None):
     """
     Split the data randomly into train and test sets
     @param x: input data as pandas dataframe
     @param y: ground truth data (multiple columns: groups, rpe, name)
     @param train_percentage: percentage value of train subjects
-    @param random_seed: flag if a random seed should be used to always generate same split
-    @return: tuple with (x_train, y_train, x_test, y_test)
+    @param random_seed: a random seed as integer, if not set, use a random split
+    @return: tuple with four pandas data frames (x_train, y_train, x_test, y_test)
     """
     subject_names = sorted(y['name'].unique())
     nr_subjects = int(len(subject_names) * train_percentage)
 
-    if random_seed:
-        np.random.seed(42)
+    if random_seed is not None:
+        np.random.seed(random_seed)
 
     train_subjects = np.random.choice(subject_names, nr_subjects, replace=False)
     train_indices = y['name'].isin(train_subjects)
