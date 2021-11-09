9fe92000b4671b9f01f5f97574aa5b27bae27c7a
Justin <mail@justin-albert.com>
2021-11-09 15:05:25 +0100

    Removed obsolete config_reader.py and rendering package.

--- rpe_prediction/config/__init__.py
+++ rpe_prediction/config/__init__.py
@@ -1,4 +1,3 @@
-from .config_reader import ConfigReader
 from .utils import read_parquet_sensor_data, resort_file
 from .raw_data_iterator import SubjectDataIterator
 from .data_loaders import SubjectDataCollector, FusedAzureSubjectLoader, RPESubjectLoader, StereoAzureSubjectLoader
