5bc972cee35f0263c7a48a3fd96c0df4590feef8
Justin Albert <justin.albert@hpi.de>
2023-01-06 16:38:05 +0100

    Implemented TensorBoard support.

--- train_dl.py
+++ train_dl.py
@@ -4,7 +4,9 @@ from argparse import ArgumentParser
 
 import pandas as pd
 import tensorflow as tf
+import datetime
 import os
+
 
 parser = ArgumentParser()
 parser.add_argument("--win_size", type=int, dest="win_size", default=60)
@@ -36,7 +38,16 @@ test_gen = DataSetIterator(X_test, y_test, win_size=args.win_size, overlap=0.9, 
 
 print(train_gen, test_gen)
 
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+
 model = build_fcn_regression_model(n_samples=60, n_features=96)
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss="mse", metrics=["mse"])
 model.summary()
-model.fit(train_gen, validation_data=test_gen, epochs=10)
+model.fit(
+    train_gen,
+    validation_data=test_gen,
+    epochs=10,
+    callbacks=[tensorboard_callback]
+)
