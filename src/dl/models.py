1f34bea1cb96a238b1db540973a4ed5096913982
Justin Albert <justin.albert@hpi.de>
2023-11-30 09:11:46 +0100

    Started to add a simple siamese network.

--- src/dl/models.py
+++ src/dl/models.py
@@ -12,22 +12,23 @@ from keras.layers import (
     concatenate,
 )
 
-def build_dual_cnn_lstm_model(hp, win_size: int, n_features: int):
-    input1 = Input(shape=(win_size, n_features,))
-    input2 = Input(shape=(win_size, n_features,))
-    model = concatenate([input1, input2])
+
+def build_dual_cnn_lstm_model(hp, win_size_1: int, n_features_1: int, win_size_2: int, n_features_2: int):
+    input1 = Input(shape=(win_size_1, n_features_1,))
+    input2 = Input(shape=(win_size_2, n_features_2,))
 
     for i in range(hp.Choice('n_layers', values=[2, 3])):
-        model.add(Conv1D(
+        input1.add(Conv1D(
             filters=hp.Choice('filters_1', values=[32, 64]) * (2 ** i),
             kernel_size=hp.Choice('kernel_size_1', values=[3, 7, 11]),
             padding="valid",
             activation="relu",
             kernel_regularizer=l2(0.01),
         ))
-        model.add(BatchNormalization())
-        model.add(MaxPooling1D(pool_size=2))
+        input1.add(BatchNormalization())
+        input2.add(MaxPooling1D(pool_size=2))
 
+    model = concatenate([input1, input2])
     model.add(GRU(hp.Choice("gru_units", values=[8, 16, 32, 64, 128]), activation="tanh", return_sequences=False))
     model.add(Dense(1, activation="linear"))
 
@@ -35,7 +36,6 @@ def build_dual_cnn_lstm_model(hp, win_size: int, n_features: int):
         optimizer=keras.optimizers.Adam(learning_rate=hp.Float("learning_rate", min_value=1e-4, max_value=1e-2)),
         loss="mse", metrics=["mse", "mae", "mape", RSquare()]
     )
-    # model.summary()
     return model
 
 
