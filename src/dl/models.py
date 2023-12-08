a039db4e92600afbd1ff39f8dcb7743a5ec94b11
Justin Albert <justin.albert@hpi.de>
2023-12-08 10:18:33 +0100

    Fixed fusion experiment config. Extracted CNN backbone code to own function. Fixed evaluation script.

--- src/dl/models.py
+++ src/dl/models.py
@@ -16,25 +16,11 @@ from keras.layers import (
 
 
 def build_cnn_fc_model(hp, win_size, n_features):
-    model = keras.Sequential()
-    model.add(Input(shape=(win_size, n_features)))
-
-    for i in range(hp.Choice('n_layers', values=[2, 3])):
-        model.add(Conv1D(
-            filters=hp.Choice(f"n_filters", values=[32, 64]) * (2 ** i),
-            kernel_size=hp.Choice(f"kernel_size", values=[3, 7, 11]),
-            padding="valid",
-            activation=None,
-            kernel_regularizer=l2(0.01),
-        ))
-        model.add(BatchNormalization())
-        model.add(MaxPooling1D(pool_size=2))
-        model.add(Activation("relu"))
-
+    model = build_cnn_backbone(hp, win_size, n_features)
     model.add(Flatten())
-    model.add(Dense(hp.Choice("fc_units_1", values=[32, 64, 128, 256]), activation="relu"))
+    model.add(Dense(hp.Choice("fc_units_1", values=[32, 64, 128]), activation="relu"))
     model.add(Dropout(hp.Float("dropout", min_value=0.0, max_value=0.5)))
-    model.add(Dense(hp.Choice("fc_units_2", values=[32, 64, 128, 256]), activation="relu"))
+    model.add(Dense(hp.Choice("fc_units_2", values=[32, 64, 128]), activation="relu"))
     model.add(Dropout(hp.Float("dropout", min_value=0.0, max_value=0.5)))
     model.add(Dense(1, activation="linear"))
 
@@ -46,21 +32,7 @@ def build_cnn_fc_model(hp, win_size, n_features):
 
 
 def build_cnn_lstm_model(hp, win_size, n_features):
-    model = keras.Sequential()
-    model.add(Input(shape=(win_size, n_features)))
-
-    for i in range(hp.Choice('n_layers', values=[2, 3])):
-        model.add(Conv1D(
-            filters=hp.Choice(f"n_filters", values=[32, 64]) * (2 ** i),
-            kernel_size=hp.Choice(f"kernel_size", values=[3, 7, 11]),
-            padding="valid",
-            activation=None,
-            kernel_regularizer=l2(0.01),
-        ))
-        model.add(BatchNormalization())
-        model.add(MaxPooling1D(pool_size=2))
-        model.add(Activation("relu"))
-
+    model = build_cnn_backbone(hp, win_size, n_features)
     model.add(LSTM(hp.Choice("gru_units", values=[32, 64, 128]), activation="tanh", return_sequences=False))
     model.add(Dense(1, activation="linear"))
 
@@ -72,6 +44,18 @@ def build_cnn_lstm_model(hp, win_size, n_features):
 
 
 def build_cnn_gru_model(hp, win_size, n_features):
+    model = build_cnn_backbone(hp, win_size, n_features)
+    model.add(GRU(hp.Choice("gru_units", values=[32, 64, 128]), activation="tanh", return_sequences=False))
+    model.add(Dense(1, activation="linear"))
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=hp.Float("learning_rate", min_value=1e-4, max_value=1e-2)),
+        loss="mse", metrics=["mse", "mae", "mape", RSquare()]
+    )
+    return model
+
+
+def build_cnn_backbone(hp, win_size, n_features):
     model = keras.Sequential()
     model.add(Input(shape=(win_size, n_features)))
 
@@ -87,11 +71,4 @@ def build_cnn_gru_model(hp, win_size, n_features):
         model.add(MaxPooling1D(pool_size=2))
         model.add(Activation("relu"))
 
-    model.add(GRU(hp.Choice("gru_units", values=[32, 64, 128]), activation="tanh", return_sequences=False))
-    model.add(Dense(1, activation="linear"))
-
-    model.compile(
-        optimizer=keras.optimizers.Adam(learning_rate=hp.Float("learning_rate", min_value=1e-4, max_value=1e-2)),
-        loss="mse", metrics=["mse", "mae", "mape", RSquare()]
-    )
     return model
--- train_dl.py
+++ train_dl.py
@@ -6,11 +6,11 @@ import os
 import matplotlib
 
 from typing import Dict
-from src.dataset import dl_normalize_data_3d_subject, aggregate_results, normalize_labels_min_max
+from src.dataset import dl_normalize_data_3d_subject, aggregate_results
 from src.dl import DLOptimization
 from datetime import datetime
 from argparse import ArgumentParser
-from os.path import join, exists
+from os.path import join, exists, isfile
 
 from src.plot import (
     plot_sample_predictions,
@@ -32,30 +32,38 @@ def train_time_series_grid_search(X: np.ndarray, y: pd.DataFrame, dst_path: str,
 def evaluate_result_grid_search(src_path: str, aggregate: bool = False):
     dst_path = src_path.replace("train", "test")
     results_df = collect_trials(src_path)
-    # results_df.rename(columns={"set_id": "ground_truth", "rpe": "set_id", "predictions": "prediction"}, inplace=True)
-    results_df.rename(columns={"predictions": "prediction"}, inplace=True)
-    plot_sample_predictions(results_df, "rpe", join(dst_path, "plots"))
 
-    if aggregate:
-        results_df["model"] = "CNN-GRU"
-        results_df = aggregate_results(results_df)
+    for model in results_df["model"].unique():
+        model_df = results_df[results_df["model"] == model]
+        cur_path = join(dst_path, model)
+        os.makedirs(cur_path, exist_ok=True)
 
-    create_bland_altman_plot(results_df, join(dst_path), "CNN", "rpe")
-    create_scatter_plot(results_df, dst_path, "CNN", "rpe")
-    create_residual_plot(results_df, dst_path, "CNN")
-    train_df = create_retrain_table(results_df, dst_path)
-    train_df.to_csv(join(dst_path, "retrain.csv"))
+        plot_sample_predictions(model_df, "rpe", join(cur_path, "plots"))
+
+        if aggregate:
+            model_df = aggregate_results(model_df)
+
+        create_bland_altman_plot(model_df, join(cur_path), "CNN", "rpe")
+        create_scatter_plot(model_df, cur_path, "CNN", "rpe")
+        create_residual_plot(model_df, cur_path, "CNN")
+        train_df = create_retrain_table(model_df, cur_path)
+        train_df.to_csv(join(cur_path, "retrain.csv"))
 
 
 def collect_trials(src_path: str) -> pd.DataFrame:
     result_df = pd.DataFrame()
-    for folder in os.listdir(src_path):
-        result_file = join(src_path, folder, "eval_dataset.csv")
-        if not exists(result_file):
+    for model_folder in os.listdir(src_path):
+        if isfile(join(src_path, model_folder)):
             continue
 
-        df = pd.read_csv(result_file, index_col=0)
-        result_df = pd.concat([result_df, df])
+        for folds in os.listdir(join(src_path, model_folder)):
+            result_file = join(src_path, model_folder, folds, "eval_dataset.csv")
+            if not exists(result_file):
+                continue
+
+            df = pd.read_csv(result_file, index_col=0)
+            df["model"] = model_folder
+            result_df = pd.concat([result_df, df])
 
     return result_df
 
@@ -63,12 +71,12 @@ def collect_trials(src_path: str) -> pd.DataFrame:
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--src_path", type=str, dest="src_path", default="data/training")
-    parser.add_argument("--eval_path", type=str, dest="eval_path", default="20231206-082601_kinect")
+    parser.add_argument("--eval_path", type=str, dest="eval_path", default="Kinect")
     parser.add_argument("--dst_path", type=str, dest="dst_path", default="results/dl/train")
     parser.add_argument("--exp_path", type=str, dest="exp_path", default="experiments/dl")
     parser.add_argument("--restore_path", type=str, dest="restore_path", default=None)
-    parser.add_argument("--exp_file", type=str, dest="exp_file", default="rpe_kinect.yaml")
-    parser.add_argument("--train", type=bool, dest="train", default=True)
+    parser.add_argument("--exp_file", type=str, dest="exp_file", default="rpe_both.yaml")
+    parser.add_argument("--train", type=bool, dest="train", default=False)
     parser.add_argument("--eval", type=bool, dest="eval", default=True)
     parser.add_argument("--use_gpu", type=bool, dest="use_gpu", default=True)
     args = parser.parse_args()
