76c5b8ce6ec663e302fda1d20b72dfb777f37b83
Justin Albert <justin.albert@hpi.de>
2024-03-11 15:45:28 +0100

    Updated plotting scripts for segmentation and fusion.

--- src/scripts/plot_repetition.py
+++ src/scripts/plot_repetition.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 from src.plot import plot_settings as ps
 from src.processing import segment_kinect_signal
 from src.processing import apply_butterworth_1d_signal
+from argparse import ArgumentParser
+from os.path import join
 
 
-def plot_segmentation(pos_df: pd.DataFrame):
+def plot_segmentation(pos_df: pd.DataFrame, file_name: str):
     pelvis = pos_df["PELVIS (y)"]
     pelvis_smooth = apply_butterworth_1d_signal(pelvis, cutoff=12, sampling_rate=30, order=4)
     pelvis_smooth = pd.Series(pelvis_smooth, index=pelvis.index)
@@ -17,35 +19,40 @@ def plot_segmentation(pos_df: pd.DataFrame):
     eccentric, full_repetitions = segment_kinect_signal(
         pelvis, prominence=0.01, std_dev_p=0.4, min_dist_p=0.5, min_time=30, mode="eccentric", show=False)
 
-    fig, (axs1, axs2) = plt.subplots(2, 1, figsize=(ps.text_width * ps.cm, ps.text_width * 0.5 * ps.cm), dpi=ps.dpi)
-    axs1.plot(pelvis, color="black")
-    axs1.set_ylabel("Distance (m)")
-    axs1.set_xlabel("Time (s)")
-    ymin, ymax = axs1.get_ylim()
+    colors = ps.get_colors(3)
+    plt.subplots(figsize=(ps.TEXT_WIDTH_INCH, ps.TEXT_WIDTH_INCH * 0.45), dpi=ps.DPI)
+    plt.plot(pelvis, color=colors[0])
+
+    # axs1.set_ylabel("Distance (m)")
+    # axs1.set_xlabel("Time (s)")
+    # ymin, ymax = axs1.get_ylim()
 
     # Horizontal lines
-    for start, end in full_repetitions:
-        axs1.vlines(start, ymin=ymin, ymax=ymax, color="gray", alpha=1)
-    axs1.vlines(full_repetitions[-1][1], ymin=ymin, ymax=ymax, color="gray", alpha=1)
+    # for start, end in full_repetitions:
+    #     axs1.vlines(start, ymin=ymin, ymax=ymax, color="gray", alpha=1)
+    # axs1.vlines(full_repetitions[-1][1], ymin=ymin, ymax=ymax, color="gray", alpha=1)
 
     for i, (start, end) in enumerate(concentric):
-        axs2.axvspan(start, end, color="0.1", alpha=0.3, label="Concentric" if i == 0 else None)
+        plt.axvspan(start, end, color=colors[1], alpha=0.3, label="Concentric" if i == 0 else None)
 
     for i, (start, end) in enumerate(eccentric):
-        axs2.axvspan(start, end, color="0.7", alpha=0.3, label="Eccentric" if i == 0 else None)
+        plt.axvspan(start, end, color=colors[2], alpha=0.3, label="Eccentric" if i == 0 else None)
 
-    axs2.plot(pelvis_smooth, color="0.0")
-    axs2.set_ylabel("Distance (m)")
-    axs2.set_xlabel("Time (s)")
-    axs2.legend(loc="upper right")
+    # axs2.plot(pelvis_smooth, color="0.0")
+    plt.ylabel("Distance [cm]")
+    plt.xlabel("Time [hh:mm:ss]")
+    plt.legend(loc="upper right")
 
     plt.tight_layout()
-    plt.savefig("segmentation.pdf", dpi=ps.dpi)
+    plt.savefig(file_name, bbox_inches='tight', pad_inches=ps.CUT_OFF, dpi=ps.DPI)
     plt.close()
 
 
 if __name__ == "__main__":
-    matplotlib.use("WebAgg")
+    parser = ArgumentParser()
+    parser.add_argument("--src_path", type=str, dest="src_path", default="../../plots")
+    args = parser.parse_args()
+
     pos_df = pd.read_csv("../../data/processed/927394/08_set/pos.csv", index_col=0)
     pos_df.index = pd.to_datetime(pos_df.index)
-    plot_segmentation(pos_df)
+    plot_segmentation(pos_df, join(args.src_path, "segmentation.pdf"))
--- src/scripts/sync_plot.py
+++ src/scripts/sync_plot.py
@@ -6,6 +6,7 @@ import matplotlib.dates as md
 from src.plot import plot_settings as ps
 from argparse import ArgumentParser
 from PySkeletonFitter import fuse_multiple_skeletons, fit_inverse_kinematic_parallel
+from os.path import join
 
 
 def plot_sync_data(pos_df: pd.DataFrame, imu_df: pd.DataFrame):
@@ -16,7 +17,7 @@ def plot_sync_data(pos_df: pd.DataFrame, imu_df: pd.DataFrame):
     pos_shift_df.index = pos_shift_df.index + pd.Timedelta(seconds=-3.5)
 
     xfmt = md.DateFormatter("%M:%S")
-    fix, axs = plt.subplots(2, 1, figsize=(ps.text_width * ps.cm, ps.text_width * 0.6 * ps.cm), dpi=ps.dpi)
+    fix, axs = plt.subplots(2, 1, figsize=(ps.TEXT_WIDTH_INCH, ps.TEXT_WIDTH_INCH * 0.6), dpi=ps.DPI)
 
     # Original
     lns1 = axs[0].plot(imu_df["CHEST_ACCELERATION_Z"], color="black", label="CHEST IMU")
@@ -47,37 +48,39 @@ def plot_sync_data(pos_df: pd.DataFrame, imu_df: pd.DataFrame):
     axs[1].set_ylabel("Acceleration [m/$s^2$]")
 
     plt.tight_layout()
-    plt.savefig("sync.pdf", dpi=300)
+    plt.savefig("sync.pdf", bbox_inches='tight', pad_inches=ps.CUT_OFF, dpi=ps.DPI)
 
 
-def fusing_plots(master_path: str, sub_path: str, joint_col: str):
+def fusing_plots(master_path: str, sub_path: str, joint_col: str, src_path: str):
     master_df = pd.read_csv(master_path, sep=";", index_col=0)
     sub_df = pd.read_csv(sub_path, sep=";", index_col=0)
     df1, df2, fused_df = fuse_multiple_skeletons(master_df, sub_df)
     fused_df.index = df1.index
+    colors = ps.get_colors(3)
 
-    plt.figure(figsize=(ps.text_width * ps.cm, ps.text_width * 0.5 * ps.cm), dpi=ps.dpi)
-    plt.plot(df1[joint_col], label="Left")
-    plt.plot(df2[joint_col], label="Right")
-    plt.plot(fused_df[joint_col], label="Fused")
+    plt.figure(figsize=(ps.TEXT_WIDTH_INCH, ps.TEXT_WIDTH_INCH * 0.5), dpi=ps.DPI)
+    plt.plot(df1[joint_col], label="Left", color=colors[0])
+    plt.plot(df2[joint_col], label="Right", color=colors[1])
+    plt.plot(fused_df[joint_col], label="Fused", color=colors[2])
 
     plt.xlabel("Time [s]")
     plt.ylabel("Position [m]")
-    plt.legend(loc="upper right")
+    plt.legend(loc=0)
     plt.tight_layout()
-    plt.savefig("fusing.pdf", dpi=ps.dpi)
+    plt.savefig(join(src_path, "fusing.pdf"), bbox_inches='tight', pad_inches=ps.CUT_OFF, dpi=ps.DPI)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--src_path", type=str, dest="src_path", default="plots")
+    parser.add_argument("--src_path", type=str, dest="src_path", default="../../plots")
     args = parser.parse_args()
 
     matplotlib.use("WebAgg")
     fusing_plots(
         "../../../../PERSIST/subject_04/azure/master_1/positions_3d.csv",
         "../../../../PERSIST/subject_04/azure/sub_1/positions_3d.csv",
-        joint_col="KNEE_RIGHT (y)"
+        joint_col="KNEE_RIGHT (y)",
+        src_path=args.src_path,
     )
 
     pos_df = pd.read_csv("../../data/processed/37A7AA/10_set/pos.csv", index_col=0)
